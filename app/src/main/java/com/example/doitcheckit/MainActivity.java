97ff61d944892bb7770344e610246f14aefcd365
LailaAT <89098933+LailaAT@users.noreply.github.com>
2021-09-25 21:09:02 +0100

    countdown2

--- app/src/main/java/com/example/doitcheckit/MainActivity.java
+++ app/src/main/java/com/example/doitcheckit/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.doitcheckit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -8,8 +9,10 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 
 import com.example.doitcheckit.Adapter.ToDoAdapter;
 import com.example.doitcheckit.Model.TasksModel;
@@ -33,11 +36,17 @@ public class MainActivity extends AppCompatActivity implements DialogCloseListen
     private Database db;
     private TaskDAO taskDAO;
 
+    private CardView taskView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+
+        //taskView = (CardView)getLayoutInflater().inflate(R.layout.task_layout, CardView , true);
+        //countdown = new RelativeLayout(this, android.R.layout.countdown);
+        //LinearLayout ll = (LinearLayout)getLayoutInflater().inflate(R.layout.whatever_name_you_gave_it, theParentViewGroup, true);
 
         start = (Button) findViewById(R.id.startButton);
         start.setOnClickListener(new View.OnClickListener() {
